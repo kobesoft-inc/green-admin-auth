5ce6de80feeead4eb87e00ae7282027e8b37a9c5
Toshimasa Matsuoka <matsuoka@entap.co.jp>
2023-11-03 21:15:10 +0900

    HasGetOptionsを追加

---
+++ src/Traits/HasGetOptions.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Green\AdminBase\Traits;
+
+use Closure;
+use Illuminate\Database\Eloquent\Model;
+
+class HasGetOptions
+{
+    /**
+     * 並び順のカラム
+     */
+    const SORT_ORDER = 'sort_order';
+
+    /**
+     * 選択肢のラベルのカラム
+     */
+    const TITLE = 'name';
+
+    /**
+     *  このモデルの選択肢を取得する
+     *
+     * @param  Closure|null  $closure  条件を加えるためのクロージャ
+     * @return array<string, string>
+     */
+    static public function getOptions(Closure $closure = null): array
+    {
+        $primaryKey = (new static())->primaryKey;
+        $query = static::query();
+        if ($closure) {
+            $query = $closure($query);
+        }
+        return $query
+            ->orderBy(self::SORT_ORDER)
+            ->pluck(static::TITLE, $primaryKey)
+            ->toArray();
+    }
+}
